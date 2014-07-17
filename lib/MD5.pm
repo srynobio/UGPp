ccdc837bea1ed5ab86d1417287896f3986f022a8
Shawn Rynearson <shawn.rynearson@gmail.com>
2014-07-17 16:55:53 -0600

    Update MD5.pm
    Added some magic to md5 class.

--- lib/MD5.pm
+++ lib/MD5.pm
@@ -21,6 +21,15 @@ sub MD5_checksum {
         chomp $file;
         next unless ( $file =~ /md5_checksums.txt/ );
 
+        my $path = $opts->{data};
+
+        # add file path to file.
+        my $file_update = "perl -lane '\$F[1] =~ s?^?  $path?; print \@F' $file > tmp.md5";
+        if ( $tape->execute ) {
+                `$file_update`;
+                `mv tmp.md5 $file`;
+        }
+
         my $cmd = sprintf(
             "md5sum -c %s", $file
         );
